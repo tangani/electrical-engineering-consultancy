88ccefdca0e52030d99ecfae6ea32250acb8feb6
Tangani Moyo <tanganimoyo@gmail.com>
2021-01-26 11:34:42 +0200

    Main company image with overlay text in a carousel

---
+++ electrical-supplies-frontend/src/app/components/home/top-image/top-image.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-image',
+  templateUrl: './top-image.component.html',
+  styleUrls: ['./top-image.component.scss']
+})
+export class TopImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+// style="width:100%;height:500px;"
